0d0b5892a822a21a7b1dbb420be4c50914257732
ผศ.ดร. เอกรัฐ รัฐกาญจน์ <69342162+ekaratnida@users.noreply.github.com>
2025-03-07 15:31:52 +0700

    Update anim.py

--- PyGame/day05/anim.py
+++ PyGame/day05/anim.py
@@ -19,26 +19,53 @@ class MySprite(pygame.sprite.Sprite):
             self.images.append(pygame.image.load('day5/Sprites/images/walk9.png'))
             self.images.append(pygame.image.load('day5/Sprites/images/walk10.png'))
 
+            for i in range(len(self.images)):
+                self.images[i] = pygame.transform.scale(self.images[i], (100,120))
+
             self.index = 0
             self.image = self.images[self.index]
             self.rect = pygame.Rect(5,5,150,198)
-
+    
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+ 
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+    
+    def moveForward(self, speed):
+        self.rect.y += speed * speed/10
+ 
+    def moveBack(self, speed):
+        self.rect.y -= speed * speed/10
+ 
     def update(self):
         self.index += 1
         if self.index >= len(self.images):
             self.index = 0
         self.image = self.images[self.index]
-
+       
 def main():
      pygame.init()
      screen = pygame.display.set_mode(SIZE)
      my_sprite = MySprite()
      my_group = pygame.sprite.Group(my_sprite)
      clock = pygame.time.Clock()
-     while True:
+     run = True
+     while run:
         for event in pygame.event.get():
-             if event.type == pygame.QUIT:
-                 break
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            my_sprite.moveLeft(10)
+        if keys[pygame.K_RIGHT]:
+            my_sprite.moveRight(10)
+        if keys[pygame.K_DOWN]:
+            my_sprite.moveForward(10)
+        if keys[pygame.K_UP]:
+            my_sprite.moveBack(10)
         
         my_group.update()
         screen.fill(BACKGROUND_COLOR)
